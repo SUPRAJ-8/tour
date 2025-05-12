564997752b9d0a3ba84fb877d30e8af4a7b75c8e
SUPRAJ-8 <supraj@example.com>
2025-05-12 10:12:30 +0545

    re make

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Running git add .
-git add .
+"C:\Program Files\Git\bin\git.exe" add .
 
 echo.
 echo Enter your commit message:
@@ -8,11 +8,11 @@ set /p commit_message=
 
 echo.
 echo Running git commit -m "%commit_message%"
-git commit -m "%commit_message%"
+"C:\Program Files\Git\bin\git.exe" commit -m "%commit_message%"
 
 echo.
 echo Running git push
-git push
+"C:\Program Files\Git\bin\git.exe" push
 
 echo.
 echo All done!
